15d4a671141a0fdf8b485e28ee324aca8d73afbe
Oliver Sharif <spockmob@gmail.com>
2020-04-23 20:40:42 +0200

    Just a starting Message

---
+++ src/de/wuh/Main.java
@@ -0,0 +1,8 @@
+package de.wuh;
+
+public class Main {
+    // Main Startingpoint of the Kernel
+    public static void main(String[] args) {
+        System.out.println("Started WUH_KERNEL!");
+    }
+}
